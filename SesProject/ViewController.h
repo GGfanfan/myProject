9886bcded51ebba51af9b125de533d1ff9d8154e
ll <ll@qq.com>
2015-07-20 17:13:36 +0800

    Initial Commit

---
+++ SesProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SesProject
+//
+//  Created by ses on 15/7/20.
+//  Copyright (c) 2015年 上海壹嫂网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SesProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SesProject
+//
+//  Created by ses on 15/7/20.
+//  Copyright (c) 2015年 上海壹嫂网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
