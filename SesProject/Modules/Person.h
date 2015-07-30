d2ad1b546f6b16e0ad53ddc9b475b1e7894461d1
fantianmin <fantianminios@sina.com>
2015-07-30 17:20:30 +0800

    MagicalRecord

---
+++ SesProject/Modules/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  
+//
+//  Created by ses on 15/7/30.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Person : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+
+@end
---
+++ SesProject/Modules/Person.m
@@ -0,0 +1,16 @@
+//
+//  Person.m
+//  
+//
+//  Created by ses on 15/7/30.
+//
+//
+
+#import "Person.h"
+
+
+@implementation Person
+
+@dynamic name;
+
+@end
