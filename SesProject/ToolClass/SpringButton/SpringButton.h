ad40bc76a5b41ebe13a0850e56be52fbca54f9a6
fantianmin <fantianminios@sina.com>
2015-07-31 17:40:56 +0800

    弹簧按钮

---
+++ SesProject/ToolClass/SpringButton/SpringButton.h
@@ -0,0 +1,13 @@
+//
+//  SpringButton.h
+//  SesProject
+//
+//  Created by ses on 15/7/31.
+//  Copyright (c) 2015年 上海壹嫂网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpringButton : UIButton
+
+@end
---
+++ SesProject/ToolClass/SpringButton/SpringButton.m
@@ -0,0 +1,21 @@
+//
+//  SpringButton.m
+//  SesProject
+//
+//  Created by ses on 15/7/31.
+//  Copyright (c) 2015年 上海壹嫂网络科技有限公司. All rights reserved.
+//
+
+#import "SpringButton.h"
+
+@implementation SpringButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
